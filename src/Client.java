ffdb12520fd0fb13fb6569e05d15f054bf5dd9cd
Jiasheng <cf17jiasheng.cheng@iesjoandaustria.org>
2019-05-25 21:48:32 +0200

    Ex12: Client.java Imprementar el metode bonificacionsDeLloguer

--- src/Client.java
+++ src/Client.java
@@ -45,16 +45,7 @@ public class Client {
             getNom() +
             " (" + getNif() + ")\n";
         for (Lloguer lloguer: lloguers) {
-
-            // afegeix lloguers freqüents
-            bonificacions ++;
-
-            // afegeix bonificació per dos dies de lloguer de Luxe
-            if (lloguer.getVehicle().getCategoria() == Vehicle.LUXE &&
-                    lloguer.getDies()>1 ) {
-                bonificacions ++;
-            }
-
+            bonificacions += bonificacionsDeLloguer(lloguer);
             // composa els resultats d'aquest lloguer
             resultat += "\t" +
                 lloguer.getVehicle().getMarca() +
@@ -68,6 +59,15 @@ public class Client {
         resultat += "Import a pagar: " + total + "€\n" +
             "Punts guanyats: " + bonificacions + "\n";
         return resultat;
+    }
+    
+    public int bonificacionsDeLloguer(Lloguer lloguer) {
+        int bonificacions = 1;
+    	if (lloguer.getVehicle().getCategoria() == Vehicle.LUXE &&
+                lloguer.getDies()>1 ) {
+            bonificacions ++;
+        }
+		return bonificacions;
     }
     
 	@Override
