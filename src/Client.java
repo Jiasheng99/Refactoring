8c22885f9f2795bbff000eb12e0e61a12055e43b
Jiasheng <cf17jiasheng.cheng@iesjoandaustria.org>
2019-05-26 12:50:38 +0200

    Ex18: Client.java definir la constant EUROS_PER_UNITAT_DE_COST

--- src/Client.java
+++ src/Client.java
@@ -7,6 +7,7 @@ public class Client {
     private String nom;
     private String telefon;
     private Vector<Lloguer> lloguers;
+    private static final double EUROS_PER_UNITAT_DE_COST = 30;
 
     public Client(String nif, String nom, String telefon) {
         this.nif = nif;
@@ -48,7 +49,7 @@ public class Client {
                 lloguer.getVehicle().getMarca() +
                 " " +
                 lloguer.getVehicle().getModel() + ": " +
-                (lloguer.quantitat() * 30) + "€" + "\n";
+                (lloguer.quantitat() * EUROS_PER_UNITAT_DE_COST) + "€" + "\n";
         }
 
         // afegeix informació final
@@ -60,7 +61,7 @@ public class Client {
     public double importTotal() {
         double total = 0;
         for (Lloguer lloguer: lloguers) {
-        	total += lloguer.quantitat() * 30;
+        	total += lloguer.quantitat() * EUROS_PER_UNITAT_DE_COST;
         }
         return total;
     }
